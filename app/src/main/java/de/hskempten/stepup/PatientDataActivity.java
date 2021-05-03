b73778623ebc258a4847efcd5fac329dd5f3d6c4
Fahri Korkmaz <fahri.korkmaz@stud.hs-kempten.de>
2021-05-03 12:14:24 +0200

    Implemented UI for patient data

--- app/src/main/java/de/hskempten/stepup/PatientDataActivity.java
+++ app/src/main/java/de/hskempten/stepup/PatientDataActivity.java
@@ -3,6 +3,8 @@ package de.hskempten.stepup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class PatientDataActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class PatientDataActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_data);
+
+        // filling gender spinner
+        Spinner genderSpinner = (Spinner) findViewById(R.id.spinnerGender);
+        ArrayAdapter<CharSequence> genderAdapter = ArrayAdapter.createFromResource(this,
+                R.array.genders, android.R.layout.simple_spinner_item);
+        genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        genderSpinner.setAdapter(genderAdapter);
     }
 }
