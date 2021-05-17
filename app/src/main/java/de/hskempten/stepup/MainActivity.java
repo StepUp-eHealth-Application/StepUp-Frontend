daff4516629320845597e8d3008c5ed59b1f2159
Jonathan <jonathanruben.hilscher@stud.hs-kempten.de>
2021-05-17 15:17:16 +0200

    Gesundheitsziele setzen und aendern

--- app/src/main/java/de/hskempten/stepup/MainActivity.java
+++ app/src/main/java/de/hskempten/stepup/MainActivity.java
@@ -2,13 +2,31 @@ package de.hskempten.stepup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener(){
+           @Override
+           public void onClick(View v){
+               openActivity();
+           }
+        });
+
+    }
+    public void openActivity(){
+        Intent intent = new Intent(this, gesundheitszieleSetztenAendern.class);
+        startActivity(intent);
     }
 }
