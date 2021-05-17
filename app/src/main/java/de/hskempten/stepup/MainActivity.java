bf3ddffd306b00397e01aa92e542cb21bb7a39ae
Jonathan <jonathanruben.hilscher@stud.hs-kempten.de>
2021-05-17 15:47:21 +0200

    Beobachtungen erfassen erstellt

--- app/src/main/java/de/hskempten/stepup/MainActivity.java
+++ app/src/main/java/de/hskempten/stepup/MainActivity.java
@@ -3,12 +3,26 @@ package de.hskempten.stepup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button =  (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openActivity();
+            }
+        });
+    }
+    public void openActivity(){
+        Intent intent = new Intent(this, beobachtungen_erfassen.class);
+        startActivity(intent);
     }
 }
