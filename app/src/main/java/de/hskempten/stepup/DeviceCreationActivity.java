a9e38d5bca66dc8d226483271d583c1672266766
Fahri Korkmaz <fahri.korkmaz@stud.hs-kempten.de>
2021-05-27 21:26:29 +0200

    Implemented communcation with backend

--- app/src/main/java/de/hskempten/stepup/DeviceCreationActivity.java
+++ app/src/main/java/de/hskempten/stepup/DeviceCreationActivity.java
@@ -9,19 +9,33 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.hl7.fhir.r4.model.Annotation;
 import org.hl7.fhir.r4.model.CodeableConcept;
 import org.hl7.fhir.r4.model.Coding;
 import org.hl7.fhir.r4.model.Device;
 import org.hl7.fhir.r4.model.Reference;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.security.acl.AclNotFoundException;
 import java.util.HashMap;
 
+import javax.net.ssl.HandshakeCompletedEvent;
+
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import de.hskempten.stepup.fhir.FhirClient;
+import de.hskempten.stepup.helpers.APIEndpoints;
 import de.hskempten.stepup.preferences.Preferences;
 
 public class DeviceCreationActivity extends AppCompatActivity {
@@ -33,15 +47,23 @@ public class DeviceCreationActivity extends AppCompatActivity {
     Spinner statusSpinner;
     Spinner deviceTypeSpinner;
     EditText txtNote;
+    TextView txtResponse;
+
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device_creation);
 
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+
         txtDeviceName = findViewById(R.id.txtDeviceName);
         txtManufacturer = findViewById(R.id.txtManufacturer);
         txtNote = findViewById(R.id.txtNote);
+        txtResponse = findViewById(R.id.txtResponse);
+
+        txtResponse.setText("");
 
         // Adding choices to the status spinner
         statusSpinner = (Spinner) findViewById(R.id.spinnerStatus);
@@ -61,7 +83,9 @@ public class DeviceCreationActivity extends AppCompatActivity {
         btnCreateDevice.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String fhirServer = Preferences.loadFhirServerUrl();
+                String fhirServer = Preferences.loadFhirServerUrl(DeviceCreationActivity.this);
+                String backendServer = Preferences.loadBackendUrl(DeviceCreationActivity.this) + APIEndpoints.DEVICE;
+
                 String patientID = Preferences.loadSelectedPatientID(DeviceCreationActivity.this);
                 if (patientID == null) {
                     patientID = Preferences.loadPatientID(DeviceCreationActivity.this);
@@ -73,67 +97,47 @@ public class DeviceCreationActivity extends AppCompatActivity {
                 String deviceType = deviceTypeSpinner.getSelectedItem().toString();
                 String note = txtNote.getText().toString();
 
-                Device device = new Device();
+                HashMap<String, String> data = new HashMap<>();
+                data.put("name", deviceName);
+                data.put("manufacturer", manufacturer);
+                data.put("status", deviceStatus);
+                data.put("type", deviceType);
+                data.put("note", note);
+                data.put("patientID", patientID);
+                data.put("fhirServer", fhirServer);
 
-                // Setting status
-                HashMap<String, Device.FHIRDeviceStatus> status = new HashMap<>();
-                status.put("Aktiv", Device.FHIRDeviceStatus.ACTIVE);
-                status.put("Inaktiv", Device.FHIRDeviceStatus.INACTIVE);
-                status.put("Fehlerhaft", Device.FHIRDeviceStatus.ENTEREDINERROR);
-                status.put("Unbekannt", Device.FHIRDeviceStatus.UNKNOWN);
+                RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
 
-                device.setStatus(status.get(deviceStatus));
+                Log.i(LOG_TAG, "Backend URL: " + backendServer);
+                // Sending data to backend
+                JsonObjectRequest jsonobj = new JsonObjectRequest(Request.Method.POST, backendServer, new JSONObject(data),
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                String id = "";
+                                try {
+                                    id = response.getString("id");
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
 
-                // Setting the manufacturer
-                device.setManufacturer(manufacturer);
+                                Log.i(LOG_TAG, "onResponse: success");
+                                String message = "Gerät erfolgreich abgespeichert. ID: " + id;
+                                txtResponse.setText(message);
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Log.i(LOG_TAG, "onResponse: " + error.getMessage());
+                                String message = "Fehler beim abspeichern";
+                                txtResponse.setText(message);
+                            }
+                        }
+                ){
 
-                // Setting the device name
-                device.addDeviceName()
-                        .setName(deviceName)
-                        .setType(Device.DeviceNameType.PATIENTREPORTEDNAME);
-
-                // Setting device type
-                CodeableConcept deviceTypeConcept = new CodeableConcept();
-
-                HashMap<String, String> deviceTypeCodingValues = new HashMap<>();
-                deviceTypeCodingValues.put("Waage", "5042005");
-                deviceTypeCodingValues.put("Schrittzähler", "XXXXXX"); // TODO: find code
-
-                Coding deviceTypeCoding = new Coding();
-                deviceTypeCoding.setCode(deviceTypeCodingValues.get(deviceType));
-                deviceTypeCoding.setDisplay(deviceType);
-                deviceTypeCoding.setSystem("http://snomed.info/sct");
-
-                deviceTypeConcept.addCoding(deviceTypeCoding);
-                deviceTypeConcept.setText("Art des Gesundheitgeräts");
-
-                device.setType(deviceTypeConcept);
-
-                // Creating note for device
-                Annotation deviceNote = new Annotation();
-                deviceNote.setText(note);
-
-                // Creating reference to patient
-                Reference patientReference;
-                if (patientID != null) {
-                    patientReference = new Reference(patientID);
-                    patientReference.setType("Patient");
-
-                    deviceNote.setAuthor(patientReference);
-
-                    device.setPatient(patientReference);
-                }
-
-                // Setting note for device
-                device.addNote(deviceNote);
-
-                Log.d(LOG_TAG, "Creating FHIR client");
-
-                FhirContext ctx = FhirContext.forR4();
-                IGenericClient client = ctx.newRestfulGenericClient("http://hapi.fhir.org/baseR4/");
-
-                Device createDevice = (Device) client.create().resource(device).execute().getResource();
-                //Log.i(LOG_TAG, "onClick: test");
+                };
+                requestQueue.add(jsonobj);
             }
         });
     }
