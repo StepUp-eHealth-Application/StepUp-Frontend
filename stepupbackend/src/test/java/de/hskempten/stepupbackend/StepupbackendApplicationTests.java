ace297446d31342f8cbdb5c14701d9acd062d026
Fahri Korkmaz <fahri.korkmaz@stud.hs-kempten.de>
2021-05-27 17:01:26 +0200

    Added stepup backend

---
+++ stepupbackend/src/test/java/de/hskempten/stepupbackend/StepupbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hskempten.stepupbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StepupbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
