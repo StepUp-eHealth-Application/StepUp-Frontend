9ce88889b77054373510d614bf3f93cb0897c268
Fahri Korkmaz <fahri.korkmaz@stud.hs-kempten.de>
2021-05-27 20:38:23 +0200

    Added functionality to add a backend server

--- app/src/main/java/de/hskempten/stepup/preferences/Preferences.java
+++ app/src/main/java/de/hskempten/stepup/preferences/Preferences.java
@@ -9,6 +9,7 @@ public class Preferences {
     private static String PATIENT_ID = "PATIENT_ID";
     private static String FHIR_SERVER_URL = "FHIR_SERVER_URL";
     private static String SELECTED_PATIENT_ID = "SELECTED_PATIENT_ID";
+    private static String BACKEND_URL = "BACKEND_URL";
 
     public static String loadFhirServerUrl(Activity activity) {
         return loadStringPreference(activity, FHIR_SERVER_URL);
@@ -16,6 +17,14 @@ public class Preferences {
 
     public static void saveFhirServerUrl(String fhirServerUrl, Activity activity) {
         savePreference(fhirServerUrl, activity, FHIR_SERVER_URL);
+    }
+
+    public static String loadBackendUrl(Activity activity) {
+        return loadStringPreference(activity, BACKEND_URL);
+    }
+
+    public static void saveBackendUrl(String backendUrl, Activity activity) {
+        savePreference(backendUrl, activity, BACKEND_URL);
     }
 
     public static String loadPatientID(Activity activity) {
