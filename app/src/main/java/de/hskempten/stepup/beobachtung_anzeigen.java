24b0ece3dc5d30349c89b711193ac71f4c89c478
Daniel Heiserer <danielmartin.heiserer@stud.hs-kempten.de>
2021-05-27 15:30:09 +0200

    Mockup erstellt

---
+++ app/src/main/java/de/hskempten/stepup/beobachtung_anzeigen.java
@@ -0,0 +1,14 @@
+package de.hskempten.stepup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class beobachtung_anzeigen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beobachtung_anzeigen);
+    }
+}
