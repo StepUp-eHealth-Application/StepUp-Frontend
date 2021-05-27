df3e1367002a22de69163a5ff3bda4e3303e05e2
Fahri Korkmaz <fahri.korkmaz@stud.hs-kempten.de>
2021-05-27 18:01:29 +0200

    Added endpoints for creating a device and getting a device by id

--- stepupbackend/src/main/java/de/hskempten/stepupbackend/endpoints/v1/DeviceEndpoint.java
+++ stepupbackend/src/main/java/de/hskempten/stepupbackend/endpoints/v1/DeviceEndpoint.java
@@ -1,11 +1,13 @@
-package de.hskempten.stepupbackend.endpoints;
+package de.hskempten.stepupbackend.endpoints.v1;
 
 import de.hskempten.stepupbackend.controllers.DeviceController;
+import de.hskempten.stepupbackend.dto.DeviceDTO;
+import org.hl7.fhir.r4.model.Device;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/device")
@@ -14,6 +16,18 @@ public class DeviceEndpoint {
     @Autowired
     DeviceController deviceController;
 
+    @GetMapping("/{id}")
+    public HttpEntity<DeviceDTO> getDeviceById(@RequestParam("id") String id) {
+        System.out.println("ID: " + id);
+        DeviceDTO device = deviceController.getDeviceById(id);
+
+        return new ResponseEntity<>(device, HttpStatus.OK);
+    }
+
     @PostMapping
-    public 
+    public HttpEntity<DeviceDTO> createDevice(@RequestBody DeviceDTO device) {
+        DeviceDTO fhirDevice = deviceController.createDevice(device);
+
+        return new ResponseEntity<>(fhirDevice, HttpStatus.OK);
+    }
 }
