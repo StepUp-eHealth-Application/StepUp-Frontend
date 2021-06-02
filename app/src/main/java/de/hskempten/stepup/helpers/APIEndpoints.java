b5e7368271e2a4914a2c537a3ea5ab4bdf3ce1d1
Fahri Korkmaz <fahri.korkmaz@stud.hs-kempten.de>
2021-06-02 22:35:37 +0200

    FHIR Backend URL will be saved in backend now

--- app/src/main/java/de/hskempten/stepup/helpers/APIEndpoints.java
+++ app/src/main/java/de/hskempten/stepup/helpers/APIEndpoints.java
@@ -3,5 +3,6 @@ package de.hskempten.stepup.helpers;
 public class APIEndpoints {
 
     public static final String DEVICE = "api/v1/device/";
+    public static final String SETTINGS = "api/v1/settings/";
 
 }
