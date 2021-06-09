c5b7bce2f7b028a131b05d890500debf1db40d8c
Fahri Korkmaz <fahri.korkmaz@stud.hs-kempten.de>
2021-06-09 22:46:33 +0200

    Added choose observation type activity

---
+++ app/src/main/java/de/hskempten/stepup/ChooseObservationType.java
@@ -0,0 +1,14 @@
+package de.hskempten.stepup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChooseObservationType extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_observation_type);
+    }
+}
