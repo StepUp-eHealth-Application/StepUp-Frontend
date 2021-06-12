bffc6e0fc5f217f00c3e7e5ff2c09a9cf3a24bab
Fahri Korkmaz <fahri.korkmaz@stud.hs-kempten.de>
2021-06-12 19:56:41 +0200

    Changed to display patient data

--- app/src/main/java/de/hskempten/stepup/MenuLeActivity.java
+++ app/src/main/java/de/hskempten/stepup/MenuLeActivity.java
@@ -55,7 +55,7 @@ public class MenuLeActivity extends AppCompatActivity {
         btnPatientData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MenuLeActivity.this, PatientDataActivity.class);
+                Intent intent = new Intent(MenuLeActivity.this, DisplayPatientDataActivity.class);
                 startActivity(intent);
             }
         });
