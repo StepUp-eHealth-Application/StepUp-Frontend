af74fcf786a15dd2f7507e8daa33ee19755dee29
Fahri Korkmaz <fahri.korkmaz@stud.hs-kempten.de>
2021-06-17 12:14:22 +0200

    Added functionality to add a device

--- app/src/main/java/de/hskempten/stepup/beobachtung_erfassen_gewicht.java
+++ app/src/main/java/de/hskempten/stepup/beobachtung_erfassen_gewicht.java
@@ -5,27 +5,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import de.hskempten.stepup.helpers.APIEndpoints;
 import de.hskempten.stepup.preferences.Preferences;
 
-public class beobachtung_erfassen_gewicht extends AppCompatActivity {
+public class beobachtung_erfassen_gewicht extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private final static String TAG = "b_erfassen_gewicht";
     private final static int duration = Toast.LENGTH_SHORT;
@@ -33,20 +40,83 @@ public class beobachtung_erfassen_gewicht extends AppCompatActivity {
     EditText eTxtNumber;
     DatePicker dpObservationWeight;
     Button save;
+    Spinner deviceSpinner;
+
+    RequestQueue requestQueue;
+
+    List<String> deviceIds;
+    List<String> deviceNames;
+    String deviceId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beobachtung_erfassen_gewicht);
 
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+
         findViews();
+        initDeviceSpinner();
         initSaveButton();
+    }
+
+    private void initDeviceSpinner() {
+        String backendServer = Preferences.loadBackendUrl(getApplicationContext()) + APIEndpoints.DEVICE;
+        // Sending data to backend
+        JsonArrayRequest jsonobj = new JsonArrayRequest(Request.Method.GET, backendServer, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        deviceIds = new ArrayList<>();
+                        deviceNames = new ArrayList<>();
+
+                        for(int i = 0; i < response.length(); i++){
+                            JSONObject jresponse = null;
+                            try {
+                                jresponse = response.getJSONObject(i);
+
+                                String id = jresponse.getString("id");
+                                String name = jresponse.getString("name");
+
+                                if (deviceId == null || deviceId.isEmpty()) {
+                                    deviceId = id;
+                                }
+
+                                deviceIds.add(id);
+                                deviceNames.add(name);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                                getApplicationContext(), android.R.layout.simple_spinner_item, deviceNames);
+                        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        deviceSpinner.setAdapter(adapter);
+
+                        Toast.makeText(getApplicationContext(), "Geräte erfolgreich geladen!" , duration).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.i(TAG, "onResponse: " + error.getMessage());
+                        String message = "Geräte konnten nicht geladen werden: " + error.getMessage();
+                        Toast.makeText(getApplicationContext(), message , duration).show();
+                    }
+                }
+        ){
+
+        };
+        requestQueue.add(jsonobj);
+        deviceSpinner.setOnItemSelectedListener(this);
     }
 
     private void findViews() {
         eTxtNumber = findViewById(R.id.eTxtWeightObservation);
         dpObservationWeight = findViewById(R.id.dpWeightObservationDate);
         save = findViewById(R.id.btnSaveWeightObservation);
+        deviceSpinner = findViewById(R.id.spinnerDeviceWeight);
     }
 
     private void initSaveButton() {
@@ -104,7 +174,7 @@ public class beobachtung_erfassen_gewicht extends AppCompatActivity {
                 data.put("fhirServer", fhirServer);
                 data.put("id", id);
 
-                RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+                requestQueue = Volley.newRequestQueue(getApplicationContext());
 
                 // Sending data to backend
                 JsonObjectRequest jsonobj = new JsonObjectRequest(Request.Method.POST, backendServer, new JSONObject(data),
@@ -136,5 +206,15 @@ public class beobachtung_erfassen_gewicht extends AppCompatActivity {
                 requestQueue.add(jsonobj);
             }
         });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        deviceId = deviceIds.get(position);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
